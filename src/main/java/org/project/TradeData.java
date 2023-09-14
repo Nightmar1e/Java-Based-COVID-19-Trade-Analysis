f5a81adc593594700dc2f968ef749e6763c5fe23
Nightmar1e <walidelghalidi2001@gmail.com>
2023-09-14 09:28:08 +0200

    Used Lombok Library

--- src/main/java/org/project/TradeData.java
+++ src/main/java/org/project/TradeData.java
@@ -1,5 +1,9 @@
 package org.project;
 
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class TradeData {
     String direction;
     int year;
@@ -12,17 +16,4 @@ public class TradeData {
     long value;
     long cumulative;
 
-    public TradeData(String direction, int year, String date, String weekday, String country,
-                     String commodity, String transportationMode, String measure, long value, long cumulative) {
-        this.direction = direction;
-        this.year = year;
-        this.date = date;
-        this.weekday = weekday;
-        this.country = country;
-        this.commodity = commodity;
-        this.transportationMode = transportationMode;
-        this.measure = measure;
-        this.value = value;
-        this.cumulative = cumulative;
-    }
 }
